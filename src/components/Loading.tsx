d15d3730f1ee6fdffdb3c64cf00959ea1a43bf05
VC365 <xdood.sav@outlook.com>
2025-07-19 23:19:34 +0330

    Update Loading.tsx

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,8 +3,8 @@ import Iloading from "@/assets/img/Rocket.gif";
 function Loading() {
     return (
         <div className="h-screen items-center flex justify-center">
-            <div className="flex justify-between bg-zinc-600 px-6 py-2 rounded-2xl">
-                <img src={Iloading} alt="Loading ..." /> <span className="pt-5 ml-3">Loading</span>
+            <div className="flex justify-between bg-zinc-50 px-6 py-2 rounded-2xl">
+                <img src={Iloading} alt="Loading ..." /> <span className="pt-5 ml-3 text-zinc-950">Loading</span>
             </div>
         </div>
     );
